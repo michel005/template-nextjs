fe9b12e9b8a53ad4e648515c098f8a083fd34aa5
Michel Douglas Grigoli <mdgrigoli@hotmail.com.br>
2024-09-03 20:48:08 -0300

    testing configs

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-try {
-    if (isProd) {
-        module.exports = {
-            output: 'export',
-            assetPrefix: '/template-nextjs/',
-            basePath: '/template-nextjs',
-            images: {
-                unoptimized: true,
-            },
-        }
-    } else {
-        module.exports = {
-            images: {
-                unoptimized: true,
-            },
-        }
-    }
-} catch (error) {
-    console.error('Configuration Error:', error)
-    throw new Error('Failed to configure Next.js export settings.')
+module.exports = {
+    output: 'export',
+    assetPrefix: '/template-nextjs/',
+    basePath: '/template-nextjs',
+    images: {
+        unoptimized: true,
+    },
 }
