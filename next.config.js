488523649b39ee59b72080329a150c0e42bcac58
Michel Douglas Grigoli <mdgrigoli@hotmail.com.br>
2024-09-03 20:45:15 -0300

    Some changes in the project structure

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-if (isProd) {
-    module.exports = {
-        output: 'export',
-        assetPrefix: '/template-nextjs/',
-        basePath: '/template-nextjs',
-        images: {
-            unoptimized: true,
-        },
+try {
+    if (isProd) {
+        module.exports = {
+            output: 'export',
+            assetPrefix: '/template-nextjs/',
+            basePath: '/template-nextjs',
+            images: {
+                unoptimized: true,
+            },
+        }
+    } else {
+        module.exports = {
+            images: {
+                unoptimized: true,
+            },
+        }
     }
-} else {
-    module.exports = {
-        images: {
-            unoptimized: true,
-        },
-    }
+} catch (error) {
+    console.error('Configuration Error:', error)
+    throw new Error('Failed to configure Next.js export settings.')
 }
